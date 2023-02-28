99917f395aa4713f60caadd0c45a9150bf722316
Jin-SukKim <99wlstjr@gmail.com>
2023-02-28 23:35:23 +0900

    add two number

--- Problem_Solving/leetcode/linear_data_structure/Linked_List/Add_Two_Number_2/add_two_num.py
+++ Problem_Solving/leetcode/linear_data_structure/Linked_List/Add_Two_Number_2/add_two_num.py
@@ -19,3 +19,24 @@ class Solution:
             node = node.next
 
         return list
+
+    # 파이썬 리스트를 연결 리스트로 변환
+    def toReversedLinkedList(self, result: str) -> ListNode:
+        prev: ListNode = None
+        for r in result:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
+            
+        return node
+    
+    # 두 연결 리스트의 덧셈
+    def addTwoNumber(self, l1: ListNode, l2: ListNode) -> ListNode:
+        a = self.toList(self.reversList(l1))
+        b = self.toList(self.reverseList(l2))
+        
+        resultStr = int(''.join(str(e) for e in a)) + \
+                    int(''.join(str(e) for e in b))
+                    
+        # 최종 계산 결과 연결 리스트 변환
+        return self.toReversedLinkedList(str(resultStr))
