664d19f6e6c6dda8dd8306c4847aebb5583e5931
Jin-SukKim <99wlstjr@gmail.com>
2023-05-25 00:28:15 +0900

    leetcode

---
+++ Problem_Solving/leetcode/linear_data_structure/Stack/232_Implement_Queue_using_Stacks/queueUsingStack.py
@@ -0,0 +1,30 @@
+class MyQueue:
+
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output.pop()
+
+    def peek(self) -> int:
+        # output이 없으면 모두 재입력
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        return self.input == [] and self.output == []
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
