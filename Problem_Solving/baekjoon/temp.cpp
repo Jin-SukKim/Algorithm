388dbbc5e5b1217e4a411cfa88c31860159e12c0
Jin-SukKim <99wlstjr@gmail.com>
2023-07-04 21:15:55 +0900

    baekjoon 10951

--- Problem_Solving/baekjoon/temp.cpp
+++ Problem_Solving/baekjoon/temp.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 int main()
 {
-    std::cin.tie(nullptr); // cin만 풀어줘 입력을 먼저 한 번에 다 받는다.
     std::ios_base::sync_with_stdio(false);
-    int T, a, b;
-    std::cin >> T;
-    for (int i = 1; i <= T; i++) {
+    int a, b;
+    while (1)
+    {
         std::cin >> a >> b;
-        std::cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+        if (a == 0 && b == 0)
+            break;
+        std::cout << a + b << std::endl;
     }
     return 0;
 }
