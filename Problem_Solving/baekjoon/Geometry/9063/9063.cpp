b4de7cb8315f491aebae18522e4cf5d99e0ba048
Jin-SukKim <99wlstjr@gmail.com>
2023-07-13 23:53:50 +0900

    baekjoon

--- Problem_Solving/baekjoon/Geometry/9063/9063.cpp
+++ Problem_Solving/baekjoon/Geometry/9063/9063.cpp
@@ -8,15 +8,18 @@ int main(void)
 	int n;
 	std::cin >> n;
 
+	// 각 좌표의 x와 y 값을 받는다.
 	std::vector<int> x(n);
 	std::vector<int> y(n);
 
 	for (int i = 0; i < n; i++)
 		std::cin >> x[i] >> y[i];
 
+	// 정렬
 	std::sort(x.begin(), x.end());
 	std::sort(y.begin(), y.end());
 
+	// 가장 큰 값에서 가장 작은 좌표 값을 빼면 한 변의 한 변의 길이가 나와 곱해 넓이를 구한다.
 	std::cout << (x[n - 1] - x[0]) * (y[n - 1] - y[0]);
 
 	return 0;
