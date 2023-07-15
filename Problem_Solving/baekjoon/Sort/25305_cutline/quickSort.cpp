7c60e039b680ffd9ca4d519725a92478cf6cd8c6
Jin-SukKim <99wlstjr@gmail.com>
2023-07-15 23:51:45 +0900

    baekjoon quicksort

--- Problem_Solving/baekjoon/Sort/25305_cutline/quickSort.cpp
+++ Problem_Solving/baekjoon/Sort/25305_cutline/quickSort.cpp
@@ -2,8 +2,13 @@
 #include <vector>
 #include <algorithm>
 
-int sort(std::vector<int> &v, int& l, int& r)
+// 평균적으로 O(n log n), 최악의 경우 O(n^2)
+void quickSort(std::vector<int> &v, int l, int r)
 {
+	// best case
+	if (l >= r)
+		return;
+
 	// overflow 방지하며 중간 길이 구하기
 	int middle = l + (r - l) / 2;
 	// 왼쪽, 오른쪽 포인터
@@ -12,7 +17,8 @@ int sort(std::vector<int> &v, int& l, int& r)
 	// pivot 값
 	int pivot = v[middle];
 
-	while (pl < pr)
+	// <= 작거나 같을때까지 비교하면 left가 right보다 오른쪽에 간다.
+	while (pl <= pr)
 	{
 		// 왼쪽값이 pivot보다 작으면 왼쪽 포인터를 움직인다.
 		// pivot 값보다 큰 값을 찾을 때까지 포인터를 움직인다.
@@ -23,28 +29,19 @@ int sort(std::vector<int> &v, int& l, int& r)
 		while (pivot < v[pr])
 			pr--;
 
-		if (pl < pr)
+		if (pl <= pr) {
 			std::swap(v[pl], v[pr]);
+			pl++;
+			pr--;
+		}
 	}
-	return pr;
-}
-
-
-void quickSort(std::vector<int> &v, int l, int r)
-{
-	if (l >= r)
-		return;
-		
-	if (l < r)
-	{
-		// 중간값을 pivot으로 정해 정렬해준다.
-		int pivot = sort(v, l, r);
-		// 배열을 두개로 쪼갠다.
-		// pivot을 중심으로 왼쪽 배열
-		quickSort(v, l, pivot - 1);
-		// pivot을 중심으로 오른쪽 배열
-		quickSort(v, pivot + 1, r);
-	}
+	
+	// right포인터가 left 포인터보다 왼쪽에 있다.
+	if (l < pr)
+		quickSort(v, l, pr);
+	// left포인터가 right포인터보다 오른쪽에 있다.
+	if (pl < r)
+		quickSort(v, pl, r);
 }
 
 int main()
