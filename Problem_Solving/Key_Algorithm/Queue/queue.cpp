da1f7e866f6ed1e1b015876a957eace8108647bd
Jin-SukKim <99wlstjr@gmail.com>
2023-07-18 00:49:37 +0900

    backjoon queue

--- Problem_Solving/Key_Algorithm/Queue/queue.cpp
+++ Problem_Solving/Key_Algorithm/Queue/queue.cpp
@@ -15,27 +15,39 @@ public:
 
 		length = n + 1;
 		// rear의 위치는 빈 공간으로 둬야 queue가 꽉찼는지 아닌지 구분 가능하다.
-		queue = new int[length];
+		queue = new int[n+1];
 	}
 	~Queue()
 	{
 		delete queue;
 	}
-
 	void push(int x)
 	{
+		if (length - 1 == size())
+		{
+			std::cout << "Queue is full." << "\n";
+			return;
+		}
 		queue[r++] = x;
-    }
+		r %= length; // 나머지로 만들어 끝에 도달하면 앞으로 돌아갈 수 있도록 한다.
+	}
 
 	int pop()
 	{
 		if (empty())
 			return -1;
-		return queue[f++];
+		int top = queue[f];
+		f = (f + 1) % length;
+
+		return top;
 	}
 
 	int size()
 	{
+		// front가 rear보다 크면 rear는 한바퀴 돌아서 list의 앞에 있으므로
+		// 계산식이 달라진다.
+		if (f > r)
+			return (r - f) + length;
 		return r - f;
 	}
 
@@ -56,7 +68,8 @@ public:
 	{
 		if (empty())
 			return -1;
-		return queue[r - 1];
+		int b = r - 1 < 0 ? length - r - 1 : r - 1;
+		return queue[b];
 	}
 private:
 	int* queue;
@@ -68,7 +81,7 @@ private:
 int main()
 {
 	std::ios_base::sync_with_stdio(0);
-	// std::cin.tie(0);
+	std::cin.tie(0);
 
 	int n;
 	std::cin >> n;
