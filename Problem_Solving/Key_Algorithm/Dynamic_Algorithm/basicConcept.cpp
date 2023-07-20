b346bed90be66bcf7d512e7e6c17ac5b9828c2c9
Jin-SukKim <99wlstjr@gmail.com>
2023-07-21 00:45:21 +0900

    Dynamic Programming Concept

--- Problem_Solving/Key_Algorithm/Dynamic_Algorithm/basicConcept.cpp
+++ Problem_Solving/Key_Algorithm/Dynamic_Algorithm/basicConcept.cpp
@@ -6,6 +6,9 @@
     1. 큰 문제를 여러 개의 작은 문제로 나누어 푸는 기법
     2. 문제를 작은 문제의 정답들을 통해 구할 수 있다.
 */
+
+// Tabulation, Bottom-Up(상향식) - 반복문
+// 작은 문제부터 살펴본 후 작은 문제의 정답을 이용해 큰 문제의 정답을 찾는다.
 int fibonacci(int n)
 {
     // 피보나치의 작은 문제인 0, 1, 2의 값들을 저장해두면
@@ -23,6 +26,23 @@ int fibonacci(int n)
     return fib[n];
 }
 
+// Memoization, Top-Down(하향식) - 재귀
+// 작은 문제들을 해결했는지 확인해가며 풀어준다.
+std::vector<int> save(100);
+int fibonacci2(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    
+    if (save[n])
+        return save[n];
+    
+    save[n] = fibonacci2(n - 1) + fibonacci2(n - 2);
+    return save[n];
+
+}
 int main()
 {
 
