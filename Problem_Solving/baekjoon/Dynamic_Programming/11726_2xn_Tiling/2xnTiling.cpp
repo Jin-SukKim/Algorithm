fa008a9a9bc60ecec413fb66ee3e35f3a0443866
Jin-SukKim <99wlstjr@gmail.com>
2023-07-21 23:32:05 +0900

    baekjoon dynamic programming

--- Problem_Solving/baekjoon/Dynamic_Programming/11726_2xn_Tiling/2xnTiling.cpp
+++ Problem_Solving/baekjoon/Dynamic_Programming/11726_2xn_Tiling/2xnTiling.cpp
@@ -13,13 +13,13 @@ int main()
 	std::vector<int> dp;
 	dp.push_back(0); // 2 x 0은 0
 	dp.push_back(1); // 2 x 1은 1개
-	dp.push_back(3); // 2 x 2는 3개
+	dp.push_back(2); // 2 x 2는 2개
 
 	// 그 이상의 2 x n은 dp[i] = dp[i - 1] + dp[i - 2]의 방법의 수가 필요하다.
 	for (int i = 3; i <= n; i++)
 	{
 		// 하지만 10007로 나눈 나머지값이 필요하다.
-		dp.push_back((dp[i - 1] + dp[i - 2] * 2) % 10007);
+		dp.push_back((dp[i - 1] + dp[i - 2]) % 10007);
 	}
 
 	std::cout << dp[n];
