36fb930ce8918afecac17aa4513121966427d134
Jin-SukKim <99wlstjr@gmail.com>
2023-07-26 23:09:48 +0900

    baekjoon dynamic

--- Problem_Solving/baekjoon/temp.cpp
+++ Problem_Solving/baekjoon/temp.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 // Bottom-Up
 int main()
 {
@@ -8,7 +7,18 @@ int main()
 	std::cin.tie(0);
 	std::cout.tie(0);
 
-	
-	
+	int n;
+	std::cin >> n;
+
+	// 아무것도 없어도 1개의 경우다
+	std::vector<int> dp(n+1, 1);
+	dp[1] = 3;
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
+	}
+
+	std::cout << dp[n];
 	return 0;
 }
