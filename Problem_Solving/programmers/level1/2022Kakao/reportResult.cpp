d10f56b5036e6238e35d26e04664f38ec66c4c80
Jin-SukKim <99wlstjr@gmail.com>
2023-10-08 17:38:45 +0900

    programmers level 1

--- Problem_Solving/programmers/level1/2022Kakao/reportResult.cpp
+++ Problem_Solving/programmers/level1/2022Kakao/reportResult.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <map>
 #include <set>
-#include <iostream>
 
 using namespace std;
 
@@ -26,11 +25,9 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     // 신고당해서 정지된 결과
     for (string& id : id_list)
     {
-        int count;
-        if (reportCount[id] < k)
-            count = 0;
-        else 
-            count = 1;
+        int count = 0;
+        if (reportCount[id] >= k)
+            count++;
         
         // 신고한 인원 중 정지된 ID가 있다면 결과 메일 전송
         for (string reportID : reported[id])
